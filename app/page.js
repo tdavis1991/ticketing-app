fd17ad38f6aaa5ecf9044d485c01100e8fb49e21
tevin davis <aod.td1105@gmail.com>
2024-01-22 06:05:15 -0500

    Working on authentication

--- app/page.js
+++ app/page.js
@@ -1,40 +1,10 @@
 "use client";
-import TicketCard from "./(components)/TicketCard";
 import { useState, useEffect } from "react";
 
 const Dashboard = () => {
-  const [tickets, setTickets] = useState([]);
-
-  useEffect(() => {
-    const getTickets = async () => {
-      try {
-        const res = await fetch("http://localhost:3000/api/ticket");
-        const data = await res.json();
-
-        setTickets(data);
-        console.log("TICKETS:", tickets);
-      } catch (error) {
-        console.log("Failed to get tickets", error);
-      }
-    };
-    getTickets();
-  }, []);
-
   return (
     <div className="p-5">
-      <div className="lg:grid grid-cols-3 xl:grid-cols-4">
-        {tickets?.tickets?.map((ticket) => (
-          <TicketCard
-            key={ticket._id}
-            title={ticket.title}
-            priority={ticket.priority}
-            date={ticket.createdAt}
-            description={ticket.description}
-            status={ticket.status}
-            id={ticket._id}
-          />
-        ))}
-      </div>
+      <h1>Dashboard</h1>
     </div>
   );
 };
